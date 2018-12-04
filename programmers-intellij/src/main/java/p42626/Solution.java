c2259cf78fa707f9060d409b2643209f923eed13
Brad Lee <brad903@naver.com>
2018-12-04 21:40:07 +0900

    programmers 42626 heap을 구현하여 풀었지만 여전히 시간초과 및 실패케이스 발생(우선순위큐 적용 필요성)

--- programmers-intellij/src/main/java/p42626/Solution.java
+++ programmers-intellij/src/main/java/p42626/Solution.java
@@ -1,24 +1,57 @@
 package p42626;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        List<Integer> scovilleList = Arrays.stream(scoville).boxed().collect(Collectors.toList());
+        List<Integer> heapList = Arrays.stream(scoville).boxed().collect(Collectors.toList());
+        makeHeap(heapList);
 
-        while(Collections.min(scovilleList) < K) {
-            Collections.sort(scovilleList);
-            int temp1 = scovilleList.remove(0);
-            int temp2 = scovilleList.remove(0);
-            int newScoville = temp1 + (temp2 * 2);
-            scovilleList.add(newScoville);
-            if(newScoville < K && scovilleList.size() == 1) return -1;
+        while (!heapList.isEmpty()) {
+            int temp1 = heapList.remove(0);
+            if(temp1 >= K) return answer;
+            if(heapList.size() <= 0) return -1;
+            int temp2 = heapList.get(0);
+
+            heapList.set(0, temp1 + (temp2 * 2));
+            heapify(heapList, 0, heapList.size() - 1);
             answer++;
         }
-        return answer;
+        return -1;
+    }
+
+    void heapify(List<Integer> list, int i, int size) {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int min;
+        if (left <= size && list.get(left) < list.get(i)) {
+            min = left;
+        } else {
+            min = i;
+        }
+
+        if (right <= size && list.get(right) < list.get(min)) {
+            min = right;
+        }
+
+        if (min != i) {
+            swap(list, i, min);
+            heapify(list, min, size);
+        }
+    }
+
+    void makeHeap(List<Integer> list) {
+        for (int i = (list.size() - 1) / 2; i >= 0; i--) {
+            heapify(list, i, list.size() - 1);
+        }
+    }
+
+    void swap(List<Integer> list, int index1, int index2) {
+        int temp = list.get(index1);
+        list.set(index1, list.get(index2));
+        list.set(index2, temp);
     }
 }
--- programmers-intellij/src/test/java/p42626/SolutionTest.java
+++ programmers-intellij/src/test/java/p42626/SolutionTest.java
@@ -21,6 +21,9 @@ public class SolutionTest {
 
     @Test
     public void test2() {
+        // 0 0 1
+        // 0 1
+        // 2
         int[] scoville = {0, 0, 1};
         assertThat(problem.solution(scoville, 2)).isEqualTo(2);
     }
@@ -46,37 +49,50 @@ public class SolutionTest {
     @Test
     public void 힙_만들기() {
         int[] scoville = {1, 5, 2, 9, 12, 10};
-        List<Integer> scovilleList = new ArrayList<>();
-        for (int i = 0; i < scoville.length; i++) {
-            scovilleList.add(scoville[i]);
-            while(scovilleList.get(i / 2) > scoville[i]) {
-                swap(scovilleList, i / 2, i);
-            }
-        }
+        List<Integer> heapList = Arrays.stream(scoville).boxed().collect(Collectors.toList());
+        makeHeap(heapList);
+        print(heapList);
     }
 
     @Test
-    public void 하나_추출후_정렬() {
+    public void 힙_정렬() {
         int[] scoville = {1, 5, 2, 9, 12, 10};
-        List<Integer> heapList = makeHeap(scoville);
+        List<Integer> heapList = Arrays.stream(scoville).boxed().collect(Collectors.toList());
+        makeHeap(heapList);
 
-        while(!heapList.isEmpty()) {
-            swap(heapList, 0, heapList.size() - 1);
-            System.out.println(heapList.remove(heapList.size() - 1));  // 제일 작은 수
-            //todo 위에 것만 바꿔주는 것이 아니라 아래것도 계속 바꾸어줘야함
+        int sizeOfHeap = heapList.size() - 1;
+        for (int i = sizeOfHeap; i > 0; i--) {
+            swap(heapList, 0, i);
+            sizeOfHeap--;
+            heapify(heapList, 0, sizeOfHeap);
         }
-
+        print(heapList);
     }
 
-    List<Integer> makeHeap(int[] array) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < array.length; i++) {
-            list.add(array[i]);
-            while(list.get(i / 2) > array[i]) {
-                swap(list, i / 2, i);
-            }
+    void heapify(List<Integer> list, int i, int size) {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int min;
+        if(left <= size && list.get(left) < list.get(i)) {
+            min = left;
+        } else {
+            min = i;
         }
-        return list;
+
+        if(right <= size && list.get(right) < list.get(min)) {
+            min = right;
+        }
+
+        if(min != i) {
+            swap(list, i, min);
+            heapify(list, min, size);
+        }
+    }
+
+    void makeHeap(List<Integer> list) {
+        for (int i = (list.size() - 1) / 2; i >= 0; i--) {
+            heapify(list, i, list.size() - 1);
+        }
     }
 
     void swap(List<Integer> list, int index1, int index2) {

53b7e354512f6d3e23bc2d9e44c30d3e7dcb3ad1
Brad Lee <brad903@naver.com>
2018-12-16 23:32:09 +0900

    programmers 42627 solved(https://programmers.co.kr/learn/courses/30/lessons/42627?language=java)

--- programmers-intellij/src/main/java/p42627/Solution.java
+++ programmers-intellij/src/main/java/p42627/Solution.java
@@ -3,35 +3,70 @@ package p42627;
 import java.util.*;
 
 class Solution {
+    int minTime = 0;
+
     public int solution(int[][] jobs) {
         int answer = 0;
-        Queue<Integer> queue = new PriorityQueue();
-        Map<Integer, List<Integer>> map = new HashMap<>();
 
-        int maxStartTime = 0;
-        for (int i = 0; i < jobs.length; i++) {
-            int startTime = jobs[i][0];
-            maxStartTime = Math.max(startTime, maxStartTime);
-            int operationTime = jobs[i][1];
+        Queue<Job> queue = new PriorityQueue<>();
+        Map<Integer, Queue<Job>> map = new HashMap<>();
+        init(jobs, map);
 
-            if(!map.containsKey(startTime)) {
-                List<Integer> part = new ArrayList<>();
-                part.add(operationTime);
-                map.put(startTime, part);
-            } else {
-                map.get(startTime).add(operationTime);
-            }
-        }
+        queue.addAll(map.get(minTime));
+        map.remove(minTime);
+        Job tempJob = queue.peek();
+        int elapsedTime = tempJob.startTime;
+        int remainingTime = 0;
 
-        int finishedWork = 0;
-        int elapsedTime = 0;
-        while(finishedWork != jobs.length){
+        while(!map.isEmpty() || !queue.isEmpty()) {
             if(map.containsKey(elapsedTime)) {
+                queue.addAll(map.get(elapsedTime));
+                map.remove(elapsedTime);
+            }
 
+            if(!queue.isEmpty() && remainingTime == 0) {
+                Job job = queue.poll();
+                remainingTime = job.operationTime;
+                answer += (elapsedTime + job.operationTime - job.startTime);
             }
             elapsedTime++;
+            if(remainingTime != 0) remainingTime--;
         }
 
-        return answer;
+        return answer / jobs.length;
+    }
+
+    private void init(int[][] jobs, Map<Integer, Queue<Job>> map) {
+        minTime = jobs[0][0];
+        for (int i = 0; i < jobs.length; i++) {
+            minTime = Math.min(minTime, jobs[i][0]);
+            int startTime = jobs[i][0];
+            int operationTime = jobs[i][1];
+            Job job = new Job(startTime, operationTime);
+
+            if (!map.containsKey(startTime)) {
+                Queue<Job> part = new PriorityQueue<>();
+                part.offer(job);
+                map.put(startTime, part);
+            } else {
+                map.get(startTime).add(job);
+            }
+        }
+    }
+
+
+    class Job implements Comparable<Job> {
+        int startTime;
+        int operationTime;
+
+        public Job(int startTime, int operationTime) {
+            this.startTime = startTime;
+            this.operationTime = operationTime;
+        }
+
+        @Override
+        public int compareTo(Job o) {
+            return this.operationTime - o.operationTime;
+        }
     }
 }
--- programmers-intellij/src/test/java/p42627/SolutionTest.java
+++ programmers-intellij/src/test/java/p42627/SolutionTest.java
@@ -2,6 +2,8 @@ package p42627;
 
 import org.junit.Test;
 
+import java.util.*;
+
 import static org.junit.Assert.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,5 +14,84 @@ public class SolutionTest {
     public void test() {
         int[][] jobs = {{0, 3}, {1, 9}, {2, 6}};
         assertThat(problem.solution(jobs)).isEqualTo(9);
+    }
+
+    @Test
+    public void Job_정렬_테스트() {
+        int[][] jobs = {{0, 3}, {1, 9}, {2, 6}};
+        Queue<Job> queue = new PriorityQueue<>();
+
+        for (int i = 0; i < jobs.length; i++) {
+            Job job = new Job(jobs[i][0], jobs[i][1]);
+            queue.add(job);
+        }
+
+        printQueue(queue);
+    }
+
+    @Test
+    public void init() {
+        int[][] jobs = {{0, 3}, {1, 9}, {1, 6}, {2, 6}, {2, 7}};
+
+        Map<Integer, Queue<Job>> map = new HashMap<>();
+        Queue<Job> queue = new PriorityQueue<>();
+        init(jobs, map);
+        queue.addAll(map.get(1));
+        printQueue(queue);
+        System.out.println("--------");
+        queue.addAll(map.get(1));
+        queue.addAll(map.get(2));
+        printQueue(queue);
+
+
+    }
+
+    private void printQueue(Queue<Job> queue) {
+        while(!queue.isEmpty()) {
+            Job job = queue.poll();
+            System.out.println(job.startTime + " : " + job.operationTime);
+        }
+    }
+
+    private void print(Map<Integer, Queue<Job>> map) {
+        for (Integer integer : map.keySet()) {
+            System.out.print(integer + " : ");
+            for (Job job : map.get(integer)) {
+                System.out.print(job.operationTime + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private void init(int[][] jobs, Map<Integer, Queue<Job>> map) {
+        for (int i = 0; i < jobs.length; i++) {
+            int startTime = jobs[i][0];
+            int operationTime = jobs[i][1];
+            Job job = new Job(startTime, operationTime);
+
+            if(!map.containsKey(startTime)) {
+                Queue<Job> part = new PriorityQueue<>();
+                part.offer(job);
+                map.put(startTime, part);
+            } else {
+                map.get(startTime).add(job);
+            }
+        }
+    }
+
+    class Job implements Comparable<Job>{
+        int startTime;
+        int operationTime;
+
+        public Job(int startTime, int operationTime) {
+            this.startTime = startTime;
+            this.operationTime = operationTime;
+        }
+
+
+        @Override
+        public int compareTo(Job o) {
+            return this.operationTime - o.operationTime;
+        }
     }
 }

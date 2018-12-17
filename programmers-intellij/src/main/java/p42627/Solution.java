07f24492db575a05335833229f03438f0779102f
Brad Lee <brad903@naver.com>
2018-12-17 20:32:53 +0900

    programmers 42627 refactoring

--- programmers-intellij/src/main/java/p42627/Solution.java
+++ programmers-intellij/src/main/java/p42627/Solution.java
@@ -3,8 +3,6 @@ package p42627;
 import java.util.*;
 
 class Solution {
-    int minTime = 0;
-
     public int solution(int[][] jobs) {
         int answer = 0;
 
@@ -12,12 +10,8 @@ class Solution {
         Map<Integer, Queue<Job>> map = new HashMap<>();
         init(jobs, map);
 
-        queue.addAll(map.get(minTime));
-        map.remove(minTime);
-        Job tempJob = queue.peek();
-        int elapsedTime = tempJob.startTime;
+        int elapsedTime = 0;
         int remainingTime = 0;
-
         while(!map.isEmpty() || !queue.isEmpty()) {
             if(map.containsKey(elapsedTime)) {
                 queue.addAll(map.get(elapsedTime));
@@ -30,16 +24,14 @@ class Solution {
                 answer += (elapsedTime + job.operationTime - job.startTime);
             }
             elapsedTime++;
-            if(remainingTime != 0) remainingTime--;
+            if(remainingTime > 0) remainingTime--;
         }
 
         return answer / jobs.length;
     }
 
     private void init(int[][] jobs, Map<Integer, Queue<Job>> map) {
-        minTime = jobs[0][0];
         for (int i = 0; i < jobs.length; i++) {
-            minTime = Math.min(minTime, jobs[i][0]);
             int startTime = jobs[i][0];
             int operationTime = jobs[i][1];
             Job job = new Job(startTime, operationTime);

2f7d50fb515b916a2806e13effc4007a0810b9fe
Brad Lee <brad903@naver.com>
2018-12-31 22:50:28 +0900

    programmers 42883(https:/https://programmers.co.kr/learn/courses/30/lessons/42883) solved

--- programmers-intellij/src/main/java/p42883/Solution.java
+++ programmers-intellij/src/main/java/p42883/Solution.java
@@ -2,7 +2,21 @@ package p42883;
 
 class Solution {
     public String solution(String number, int k) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder(number);
+        while (k > 0) {
+            boolean deleted = false;
+            for (int i = 0; i < sb.length() - 1; i++) {
+                if (sb.charAt(i) < sb.charAt(i + 1)) {
+                    sb.deleteCharAt(i);
+                    deleted = true;
+                    break;
+                }
+            }
+
+            if (!deleted) sb.deleteCharAt(sb.length() - 1);
+            k--;
+        }
+
+        return sb.toString();
     }
 }
--- programmers-intellij/src/test/java/p42883/SolutionTest.java
+++ programmers-intellij/src/test/java/p42883/SolutionTest.java
@@ -1,22 +1,52 @@
 package p42883;
 
 import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SolutionTest {
+    Solution problem = new Solution();
 
     @Test
-    public void greedySelect() {
+    public void test() {
         String number = "4177252841";
         int k = 4;
 
-        List<Character> index = new ArrayList();
-        for (int i = 0; i < number.length(); i++) {
-            index.add(number.charAt(i));
+        StringBuilder sb = new StringBuilder(number);
+        while(k > 0) {
+            boolean deleted = false;
+            for (int i = 0; i < sb.length() - 1; i++) {
+                if(sb.charAt(i) < sb.charAt(i + 1)) {
+                    sb.deleteCharAt(i);
+                    deleted = true;
+                    break;
+                }
+            }
+
+            if(!deleted) sb.deleteCharAt(sb.length() - 1);
+            k--;
         }
+
+        assertThat(sb.toString().equals("775841")).isTrue();
+    }
+
+    @Test
+    public void test2() {
+        String number = "1924";
+        int k = 2;
+        assertThat(problem.solution(number, k)).isEqualTo("94");
+    }
+
+    @Test
+    public void test3() {
+        String number = "1231234";
+        int k = 3;
+        assertThat(problem.solution(number, k)).isEqualTo("3234");
+    }
+
+    @Test
+    public void test4() {
+        String number = "7777777";
+        int k = 3;
+        assertThat(problem.solution(number, k)).isEqualTo("7777");
     }
 }

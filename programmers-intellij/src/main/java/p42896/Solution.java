5b9646cb93a9a2bbf839a88e25a4cb6f4c991138
Brad Lee <brad903@naver.com>
2019-01-18 23:16:47 +0900

    programmers 42896 init

---
+++ programmers-intellij/src/main/java/p42896/Solution.java
@@ -0,0 +1,8 @@
+package p42896;
+
+class Solution {
+    public int solution(int[] left, int[] right) {
+        int answer = 0;
+        return answer;
+    }
+}
---
+++ programmers-intellij/src/test/java/p42896/SolutionTest.java
@@ -0,0 +1,8 @@
+package p42896;
+
+import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SolutionTest {
+
+}

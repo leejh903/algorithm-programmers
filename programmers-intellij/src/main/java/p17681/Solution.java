3442340f6cb17458129864ff4c0691b1791eda71
Brad Lee <brad903@naver.com>
2019-01-30 19:23:16 +0900

    programmers 17681 solved

--- programmers-intellij/src/main/java/p17681/Solution.java
+++ programmers-intellij/src/main/java/p17681/Solution.java
@@ -2,7 +2,43 @@ package p17681;
 
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = {};
+        String[] answer = new String[arr1.length];
+
+        String[] array1 = new String[arr1.length];
+        String[] array2 = new String[arr2.length];
+        for (int i = 0; i < arr1.length; i++) {
+            array1[i] = toBinary(n, arr1[i]);
+            array2[i] = toBinary(n, arr2[i]);
+            answer[i] = addBinary(array1[i], array2[i]);
+        }
         return answer;
+    }
+
+    private String addBinary(String s1, String s2) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s1.length(); i++) {
+            if(s1.charAt(i) == '0' && s2.charAt(i) == '0') {
+                sb.append(" ");
+                continue;
+            }
+            sb.append("#");
+        }
+        return sb.toString();
+    }
+
+    public String toBinary(int num, int i) {
+        int n = num - 1;
+        StringBuilder sb = new StringBuilder();
+        while(n > -1) {
+            int divider = (int)Math.pow(2, n);
+            if(i < divider) {
+                sb.append("0");
+            } else {
+                sb.append("1");
+                i = i % divider;
+            }
+            n--;
+        }
+        return sb.toString();
     }
 }
--- programmers-intellij/src/test/java/p17681/SolutionTest.java
+++ programmers-intellij/src/test/java/p17681/SolutionTest.java
@@ -3,12 +3,30 @@ package p17681;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SolutionTest {
+    Solution problem = new Solution();
 
     @Test
     public void 이진수로() {
         int a = 9;
-        System.out.println(Integer.toBinaryString(a));
+        int n = 5;
+        assertThat(problem.toBinary(n, a)).isEqualTo("01001");
+    }
+
+    @Test
+    public void 이진수로2() {
+        int a = 30;
+        int n = 5;
+        assertThat(problem.toBinary(n, a)).isEqualTo("11110");
+    }
+
+    @Test
+    public void test() {
+        int n = 5;
+        int[] arr1 = {9, 20, 28, 18, 11};
+        int[] arr2 = {30, 1, 21, 17, 28};
+        problem.solution(n, arr1, arr2);
     }
 }

461ef055997c3236027cb230b0a4afb35952c9a8
Brad Lee <brad903@naver.com>
2019-02-05 23:46:29 +0900

    programmers 17680 solved

--- programmers-intellij/src/main/java/p17680/Solution.java
+++ programmers-intellij/src/main/java/p17680/Solution.java
@@ -1,8 +1,27 @@
 package p17680;
 
+import java.util.*;
+
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
+
+        LinkedList<String> queue = new LinkedList<>();
+
+        for (int i = 0; i < cities.length; i++) {
+            String tempCity = cities[i].toLowerCase();
+            if (queue.contains(tempCity)) {
+                String theCity = queue.remove(queue.indexOf(tempCity));
+                queue.offerLast(theCity);
+                answer++;
+                continue;
+            }
+
+            if(queue.size() >= cacheSize) queue.pollFirst();
+            if(cacheSize > 0) queue.offerLast(tempCity);
+            answer += 5;
+        }
+
         return answer;
     }
 }
--- programmers-intellij/src/test/java/p17680/SolutionTest.java
+++ programmers-intellij/src/test/java/p17680/SolutionTest.java
@@ -14,4 +14,74 @@ public class SolutionTest {
         String[] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
         assertThat(problem.solution(cacheSize, cities)).isEqualTo(50);
     }
+
+    @Test
+    public void test2() {
+        int cacheSize = 3;
+        String[] cities = {"Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"};
+        assertThat(problem.solution(cacheSize, cities)).isEqualTo(21);
+    }
+
+    @Test
+    public void test3() {
+        int cacheSize = 2;
+        String[] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"};
+        assertThat(problem.solution(cacheSize, cities)).isEqualTo(60);
+    }
+
+    @Test
+    public void test4() {
+        int cacheSize = 5;
+        String[] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"};
+        assertThat(problem.solution(cacheSize, cities)).isEqualTo(52);
+    }
+
+    @Test
+    public void test5() {
+        int cacheSize = 2;
+        String[] cities = {"Jeju", "Pangyo", "NewYork", "newyork"};
+        assertThat(problem.solution(cacheSize, cities)).isEqualTo(16);
+    }
+
+    @Test
+    public void test6() {
+        int cacheSize = 0;
+        String[] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
+        assertThat(problem.solution(cacheSize, cities)).isEqualTo(25);
+    }
+
+    @Test
+    public void test6_1() {
+        int cacheSize = 0;
+        String[] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "a", "b"};
+        assertThat(problem.solution(cacheSize, cities)).isEqualTo(35);
+    }
+
+    @Test
+    public void test6_2() {
+        int cacheSize = 0;
+        String[] cities = {"Jeju", "Jeju", "Jeju", "Jeju", "Jeju", "Jeju"};
+        assertThat(problem.solution(cacheSize, cities)).isEqualTo(30);
+    }
+
+    @Test
+    public void test7() {
+        int cacheSize = 1;
+        String[] cities = {"Jeju", "Jeju", "Jeju", "Jeju", "Jeju"};
+        assertThat(problem.solution(cacheSize, cities)).isEqualTo(9);
+    }
+
+    @Test
+    public void test8() {
+        int cacheSize = 0;
+        String[] cities = {"Jeju"};
+        assertThat(problem.solution(cacheSize, cities)).isEqualTo(5);
+    }
+
+    @Test
+    public void test9() {
+        int cacheSize = 10;
+        String[] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
+        assertThat(problem.solution(cacheSize, cities)).isEqualTo(25);
+    }
 }

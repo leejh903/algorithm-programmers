10d74a2f74916f41764f83f5b7fbdb57fa704aee
Brad Lee <brad903@naver.com>
2019-02-05 18:09:59 +0900

    init programmers 17680

---
+++ programmers-intellij/src/main/java/p17680/Solution.java
@@ -0,0 +1,8 @@
+package p17680;
+
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        return answer;
+    }
+}
---
+++ programmers-intellij/src/test/java/p17680/SolutionTest.java
@@ -0,0 +1,17 @@
+package p17680;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SolutionTest {
+    Solution problem = new Solution();
+
+    @Test
+    public void test() {
+        int cacheSize = 3;
+        String[] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
+        assertThat(problem.solution(cacheSize, cities)).isEqualTo(50);
+    }
+}

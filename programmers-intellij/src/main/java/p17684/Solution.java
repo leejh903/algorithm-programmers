2b8c28399091ecb2efb004509999d76429efc556
Brad Lee <brad903@naver.com>
2019-02-18 08:57:18 +0900

    programmers 17684 sovled(45분 소요)

--- programmers-intellij/src/main/java/p17684/Solution.java
+++ programmers-intellij/src/main/java/p17684/Solution.java
@@ -1,8 +1,38 @@
 package p17684;
 
+import java.util.*;
+
 class Solution {
     public int[] solution(String msg) {
-        int[] answer = {};
-        return answer;
+        List<Integer> answer = new ArrayList<>();
+        Map<String, Integer> map = init();
+        Queue<String> queue = new LinkedList(Arrays.asList(msg.split("")));
+
+        while (!queue.isEmpty()) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(queue.peek());
+            while (map.containsKey(sb.toString()) && !queue.isEmpty()) {
+                queue.poll();
+                if(!queue.isEmpty()) sb.append(queue.peek());
+            }
+
+            if(!map.containsKey(sb.toString())) {
+                map.put(sb.toString(), map.size() + 1);
+                String temp = sb.subSequence(0, sb.length() - 1).toString();
+                answer.add(map.get(temp));
+            } else {
+                answer.add(map.get(sb.toString()));
+            }
+        }
+
+        return answer.stream().mapToInt(i->i).toArray();
+    }
+
+    public Map<String, Integer> init() {
+        Map<String, Integer> map = new HashMap<>();
+        for (char i = 'A'; i <= 'Z'; i++) {
+            map.put(String.valueOf(i), i - 'A' + 1);
+        }
+        return map;
     }
 }
--- programmers-intellij/src/test/java/p17684/SolutionTest.java
+++ programmers-intellij/src/test/java/p17684/SolutionTest.java
@@ -1,8 +1,45 @@
 package p17684;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
 import static org.junit.Assert.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SolutionTest {
+    Solution problem = new Solution();
 
+    @Test
+    public void init() {
+        Map<String, Integer> map = problem.init();
+        assertThat(map.get("A")).isEqualTo(1);
+    }
+
+    @Test
+    public void init2() {
+        Map<String, Integer> map = problem.init();
+        assertThat(map.get("K")).isEqualTo(11);
+    }
+
+    @Test
+    public void test() {
+        String msg = "KAKAO";
+        assertThat(problem.solution(msg)).isEqualTo(new int[]{11, 1, 27, 15});
+    }
+
+    @Test
+    public void test2() {
+        String msg = "TOBEORNOTTOBEORTOBEORNOT";
+        assertThat(problem.solution(msg)).isEqualTo(new int[]{20, 15, 2, 5, 15, 18, 14, 15, 20, 27, 29, 31, 36, 30, 32, 34});
+    }
+
+    @Test
+    public void test3() {
+        String msg = "ABABABABABABABAB";
+        assertThat(problem.solution(msg)).isEqualTo(new int[]{1, 2, 27, 29, 28, 31, 30});
+    }
 }

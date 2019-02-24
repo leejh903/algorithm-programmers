f656656504f0f164f435dcf8a4e0011f7465e135
Brad Lee <brad903@naver.com>
2019-02-24 22:40:16 +0900

    programmers42888 init

---
+++ programmers-intellij/src/main/java/p42888/Solution.java
@@ -0,0 +1,8 @@
+package p42888;
+
+class Solution {
+    public String[] solution(String[] record) {
+        String[] answer = {};
+        return answer;
+    }
+}
---
+++ programmers-intellij/src/test/java/p42888/SolutionTest.java
@@ -0,0 +1,15 @@
+package p42888;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SolutionTest {
+    Solution problem = new Solution();
+
+    @Test
+    public void test() {
+
+    }
+}

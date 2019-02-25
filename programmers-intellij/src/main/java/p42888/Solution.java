042cd47765c067588293a8b0a59cb4cd6622cc5b
Brad Lee <brad903@naver.com>
2019-02-25 19:23:51 +0900

    programmers 42888 solved

--- programmers-intellij/src/main/java/p42888/Solution.java
+++ programmers-intellij/src/main/java/p42888/Solution.java
@@ -1,8 +1,44 @@
 package p42888;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 class Solution {
     public String[] solution(String[] record) {
-        String[] answer = {};
-        return answer;
+        List<String> answer = new ArrayList<>();
+        Map<String, String> map = init(record);
+        print(answer, record, map);
+        return answer.toArray(new String[answer.size()]);
+    }
+
+    private void print(List<String> answer, String[] record, Map<String, String> map) {
+        for (int i = 0; i < record.length; i++) {
+            String[] split = record[i].split(" ");
+            String command = split[0];
+            String userId = split[1];
+
+            if (command.equals("Enter")) {
+                answer.add(map.get(userId) + "님이 들어왔습니다.");
+            }
+            if (command.equals("Leave")) {
+                answer.add(map.get(userId) + "님이 나갔습니다.");
+            }
+        }
+    }
+
+    public Map<String, String> init(String[] record) {
+        Map<String, String> map = new HashMap<>();
+        for (String s : record) {
+            String[] split = s.split(" ");
+            String command = split[0];
+            String userId = split[1];
+            if (command.equals("Enter") || command.equals("Change")) {
+                String name = split[2];
+                map.put(userId, name);
+            }
+        }
+        return map;
     }
 }
--- programmers-intellij/src/test/java/p42888/SolutionTest.java
+++ programmers-intellij/src/test/java/p42888/SolutionTest.java
@@ -2,6 +2,8 @@ package p42888;
 
 import org.junit.Test;
 
+import java.util.Map;
+
 import static org.junit.Assert.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -9,7 +11,16 @@ public class SolutionTest {
     Solution problem = new Solution();
 
     @Test
-    public void test() {
+    public void init() {
+        String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
+        Map<String, String> names = problem.init(record);
+        assertThat(names.get("uid1234")).isEqualTo("Prodo");
+    }
 
+    @Test
+    public void test() {
+        String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
+        String[] result = {"Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."};
+        assertThat(problem.solution(record)).isEqualTo(result);
     }
 }

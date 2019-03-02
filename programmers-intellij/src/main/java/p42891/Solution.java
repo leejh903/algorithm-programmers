2da312dc7bfe54fdafef23ad0a0897790c99d99f
Brad Lee <brad903@naver.com>
2019-03-02 20:22:09 +0900

    programmers 42891 init

---
+++ programmers-intellij/src/main/java/p42891/Solution.java
@@ -0,0 +1,4 @@
+package p42891;
+
+public class Solution {
+}
---
+++ programmers-intellij/src/test/java/p42891/SolutionTest.java
@@ -0,0 +1,14 @@
+package p42891;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SolutionTest {
+
+    @Test
+    public void test() {
+
+    }
+}

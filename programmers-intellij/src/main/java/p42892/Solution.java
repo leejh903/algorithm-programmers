4879b569fa05b962075570bfc0163c54743cc713
Brad Lee <brad903@naver.com>
2019-03-08 01:14:39 +0900

    programmers 42892 solved

--- programmers-intellij/src/main/java/p42892/Solution.java
+++ programmers-intellij/src/main/java/p42892/Solution.java
@@ -1,8 +1,105 @@
 package p42892;
 
+import java.util.*;
+
 class Solution {
     public int[][] solution(int[][] nodeinfo) {
-        int[][] answer = {};
+        Node root = injectNode(init(nodeinfo));
+        List<Integer> preList = new ArrayList<>();
+        List<Integer> postList = new ArrayList<>();
+        preOrder(preList, root);
+        postOrder(postList, root);
+        int[][] answer = {preList.stream().mapToInt(Integer::intValue).toArray(), postList.stream().mapToInt(Integer::intValue).toArray()};
         return answer;
+    }
+
+    private void postOrder(List<Integer> postList, Node node) {
+        if(node.left != null) postOrder(postList, node.left);
+        if(node.right != null) postOrder(postList, node.right);
+        postList.add(node.num);
+    }
+
+    public void preOrder(List<Integer> preList, Node node) {
+        preList.add(node.num);
+        if(node.left != null) preOrder(preList, node.left);
+        if(node.right != null) preOrder(preList, node.right);
+    }
+
+    public Queue<Node> init(int[][] nodeinfo) {
+        Queue<Node> queue = new PriorityQueue<>();
+        for (int i = 0; i < nodeinfo.length; i++) {
+            queue.add(new Node(i + 1, nodeinfo[i][0], nodeinfo[i][1]));
+        }
+        return queue;
+    }
+
+    public Node injectNode(Queue<Node> queue) {
+        Node root = queue.poll();
+        while (!queue.isEmpty()) {
+            Node thisNode = queue.poll();
+            root.add(thisNode);
+        }
+        return root;
+    }
+}
+
+class Node implements Comparable<Node> {
+    int num;
+    int x;
+    int y;
+
+    Node left;
+    Node right;
+
+    public Node(int num, int x, int y) {
+        this.num = num;
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+
+    public void add(Node node) {
+        if(node.x < this.x) {
+            if(left != null) {
+                left.add(node);
+                return;
+            }
+            left = node;
+        }
+
+        if(node.x > this.x) {
+            if(right != null) {
+                right.add(node);
+                return;
+            }
+            right = node;
+        }
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        if (o.y == this.y) {
+            return this.x - o.x;
+        }
+        return o.y - this.y;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "num=" + num +
+                ", x=" + x +
+                ", y=" + y +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
     }
 }
--- programmers-intellij/src/test/java/p42892/SolutionTest.java
+++ programmers-intellij/src/test/java/p42892/SolutionTest.java
@@ -1,7 +1,44 @@
 package p42892;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Queue;
+
 import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SolutionTest {
+    Solution problem = new Solution();
 
+    @Test
+    public void arraysSort() {
+        int[][] nodeinfo = {{5, 3}, {11, 5}, {13, 3}, {3, 5}, {6, 1}, {1, 3}, {8, 6}, {7, 2}, {2, 2}};
+        Queue<Node> queue = problem.init(nodeinfo);
+        while(!queue.isEmpty()){
+            System.out.println(queue.poll());
+        }
+    }
+
+    @Test
+    public void injectNode() {
+        int[][] nodeinfo = {{5, 3}, {11, 5}, {13, 3}, {3, 5}, {6, 1}, {1, 3}, {8, 6}, {7, 2}, {2, 2}};
+        Node root = problem.injectNode(problem.init(nodeinfo));
+        List<Integer> preList = new ArrayList<>();
+        problem.preOrder(preList, root);
+        for (Integer integer : preList) {
+            System.out.println(integer);
+        }
+    }
+
+    @Test
+    public void test() {
+        int[][] nodeinfo = {{5, 3}, {11, 5}, {13, 3}, {3, 5}, {6, 1}, {1, 3}, {8, 6}, {7, 2}, {2, 2}};
+        int[][] answer = problem.solution(nodeinfo);
+        for (int[] ints : answer) {
+            System.out.println(Arrays.toString(ints));
+        }
+    }
 }

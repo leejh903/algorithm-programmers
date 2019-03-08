08e4959c1be38235072ae5259852271fbf243139
Brad Lee <brad903@naver.com>
2019-03-08 20:21:04 +0900

    programmers 42891 solved

--- programmers-intellij/src/main/java/p42891/Solution.java
+++ programmers-intellij/src/main/java/p42891/Solution.java
@@ -1,4 +1,58 @@
 package p42891;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+class Solution {
+    public int solution(int[] food_times, long k) {
+        List<Food> foods = init(food_times);
+
+        Comparator<Food> timeComp = (o1, o2) -> o1.time - o2.time;
+
+        Comparator<Food> indexComp = (o1, o2) -> o1.index - o2.index;
+
+        foods.sort(timeComp);
+
+        int remaining = food_times.length;
+        int previous = 0;
+        int i = 0;
+        for (Food food : foods) {
+            long diff = food.time - previous;
+
+            if (diff != 0) {
+                long spend = diff * remaining;
+                if (spend <= k) {
+                    k -= spend;
+                    previous = food.time;
+                } else {
+                    k %= remaining;
+                    foods.subList(i, food_times.length).sort(indexComp);
+                    return foods.get(i + (int) k).index;
+                }
+            }
+            remaining--;
+            i++;
+        }
+
+        return -1;
+    }
+
+    List<Food> init(int[] food_times) {
+        List<Food> foods = new ArrayList<>();
+        for (int i = 0; i < food_times.length; i++) {
+            foods.add(new Food(i + 1, food_times[i]));
+        }
+        return foods;
+    }
+}
+
+class Food {
+    int index;
+    int time;
+
+    public Food(int index, int time) {
+        this.index = index;
+        this.time = time;
+    }
 }
--- programmers-intellij/src/test/java/p42891/SolutionTest.java
+++ programmers-intellij/src/test/java/p42891/SolutionTest.java
@@ -2,13 +2,39 @@ package p42891;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SolutionTest {
+    Solution problem = new Solution();
+
+    @Test
+    public void init() {
+        int[] food_times = {3, 1, 2};
+        List<Food> list = problem.init(food_times);
+        for (Food food : list) {
+            System.out.println(food);
+        }
+    }
+
+    @Test
+    public void subList() {
+        List<String> list = new ArrayList<String>();
+        list.add("bbb");
+        list.add("ccc");
+        list.add("aaa");
+
+        List<String> subList = list.subList(1, 2);
+
+        System.out.println(subList.size());
+    }
 
     @Test
     public void test() {
-
+        int[] food_times = {3, 1, 2};
+        int k = 5;
+        assertThat(problem.solution(food_times, k)).isEqualTo(1);
     }
 }

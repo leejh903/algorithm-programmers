8e5c8263569d30e8218892c20af7dee009718b57
Brad Lee <brad903@naver.com>
2019-03-08 11:39:02 +0900

    programmers 42894 푸는 중

--- programmers-intellij/src/main/java/p42894/Solution.java
+++ programmers-intellij/src/main/java/p42894/Solution.java
@@ -1,107 +1,41 @@
 package p42894;
 
-import java.util.Arrays;
 
 class Solution {
-
-    static int[][] dx = {{0, 0, 0, 1, 1, 1}, {0, 0, 1, 1, 2, 2}};
-    static int[][] dy = {{0, 1, 2, 0, 1, 2}, {0, 1, 0, 1, 0, 1}};
-    static int n;
+    int[][] drow = {{0, 0, 0, 1, 1, 1}, {0, 0, 1, 1, 2, 2}};
+    int[][] dcol = {{0, 1, 2, 0, 1, 2}, {0, 1, 0, 1, 0, 1}};
 
     public int solution(int[][] board) {
+        int n = board.length;
         int answer = 0;
+        int[][] map = createMap(board);
 
-        // n*n 2차원 배열
-        n = board.length;
-        int[][] cnt = new int[n][n];
-        int[][] chk = new int[n][n];
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                for (int i = 0; i < 2; i++) {
 
-        // 맵을 확인
-        while (true) {
-            boolean flag = false;
-            // 초기화
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    cnt[i][j] = chk[i][j] = 0;
                 }
             }
-            // 값 있는 곳에 cnt증가
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    cnt[i][j] = (board[i][j] > 0 ? 1 : 0);
-                }
-            }
-            // 해당 줄에 몇번째 블록인지 알려줌.
-            for (int i = 1; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    cnt[i][j] += cnt[i - 1][j];
-                }
-            }
-
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    for (int k = 0; k < 2; k++) {
-                        if (possi(i, j, k, cnt, board, chk)) {
-                            flag = true;
-                            answer++;
-                        }
-                    }
-                }
-            }
-
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (chk[i][j] > 0) {
-                        board[i][j] = 0;
-                    }
-                }
-            }
-
-            // 해당 while문에 깨진게 하나도 없을 경우
-            if (!flag) break;
         }
-        //System.out.println(answer);
         return answer;
     }
 
-    public static boolean possi(int x, int y, int k, int[][] cnt, int[][] board, int[][] chk) {
-        int col = -1;
-        int cur = 0;
-        for (int i = 0; i < 6; i++) {
-            int nx = x + dx[k][i];
-            int ny = y + dy[k][i];
-            if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
-                if (board[nx][ny] > 0) {
-                    if (col == -1) {
-                        col = board[nx][ny];
-                    } else {
-                        if (col != board[nx][ny]) {
-                            return false;
-                        }
-                    }
-                } else {
-                    cur++;
-                    // 위에서 깨지지 않은 블럭이 존재함을 알려줌
-                    if (cnt[nx][ny] > 0) {
-                        return false;
-                    }
-                }
-            } else {
-                return false;
+    int[][] createMap(int[][] board) {
+        int n = board.length;
+        int[][] map = new int[n][n];
+
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                if(board[row][col] > 0) map[row][col] = 1;
             }
         }
 
-        if (cur != 2) {
-            return false;
-        }
-        for (int i = 0; i < 6; i++) {
-            int nx = x + dx[k][i];
-            int ny = y + dy[k][i];
-            chk[nx][ny] = 1;
+        for (int row = 1; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                map[row][col] += map[row - 1][col];
+            }
         }
 
-        return true;
-
+        return map;
     }
-
 }
--- programmers-intellij/src/test/java/p42894/SolutionTest.java
+++ programmers-intellij/src/test/java/p42894/SolutionTest.java
@@ -2,7 +2,8 @@ package p42894;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SolutionTest {
@@ -23,5 +24,21 @@ public class SolutionTest {
         assertThat(problem.solution(board)).isEqualTo(2);
     }
 
-
+    @Test
+    public void createMap() {
+        int[][] board = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 4, 0, 0, 0},
+                {0, 0, 0, 0, 0, 4, 4, 0, 0, 0},
+                {0, 0, 0, 0, 3, 0, 4, 0, 0, 0},
+                {0, 0, 0, 2, 3, 0, 0, 0, 5, 5},
+                {1, 2, 2, 2, 3, 3, 0, 0, 0, 5},
+                {1, 1, 1, 0, 0, 0, 0, 0, 0, 5}};
+        int[][] map = problem.createMap(board);
+        for (int[] ints : map) {
+            System.out.println(Arrays.toString(ints));
+        }
+    }
 }

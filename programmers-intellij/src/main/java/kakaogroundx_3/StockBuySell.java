4e4c9b724033b07c79fa36fdefd53233eb27fc5e
Brad Lee <brad903@naver.com>
2019-03-11 22:42:56 +0900

    groundx3 sovled again

--- programmers-intellij/src/main/java/kakaogroundx_3/StockBuySell.java
+++ programmers-intellij/src/main/java/kakaogroundx_3/StockBuySell.java
@@ -5,16 +5,15 @@ class StockBuySell {
         int answer = 0;
         int n = price.length;
 
-        if (n == 1)
-            return 0;
+        if(n == 1) return 0;
 
         int index = 0;
-        while (index < n - 1) {
-            while (index < n - 1 && price[index + 1] <= price[index]) index++;
+        while(index < n - 1) {
+            while(index < n - 1 && price[index] >= price[index + 1]) index++;
 
             int buy = price[index];
-
             index++;
+
             while(index < n && price[index - 1] <= price[index]) index++;
 
             int sell = price[index - 1];

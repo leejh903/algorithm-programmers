1933d2e91747d58fdebfd11ad10b5e99c21c3e90
Brad Lee <brad903@naver.com>
2019-10-03 13:54:59 +0900

    kakao 2020 60057 wrong approach

--- programmers-vscode/2020_kakao_blind/p60057/index.js
+++ programmers-vscode/2020_kakao_blind/p60057/index.js
@@ -1,29 +1,44 @@
 function solution(s) {
     var min = s.length;
+
     for (let charLength = 1; charLength <= Math.floor(s.length / 2); charLength++) {
-        let firstHit = true;
-        let previous;
+
         let temp = 0;
-        for (let index = 0; index < Math.floor(s.length / charLength); index++) {
-            if(previous === undefined) previous = s.slice(index, index + charLength);
-            let next = s.slice(index + charLength, index + (charLength* 2));
-            console.log(`previous: ${previous}, next: ${next}, temp: ${temp}`);    
-            if(next === '' || next.length !== charLength) break;
+        let firstHit = true;
+        for (let pointer = 0; pointer < s.length; pointer++) {
+            let previous = s.slice(pointer, pointer + charLength)
+            let next = s.slice(pointer + charLength, pointer + (charLength * 2));
+
+            if (next.length < charLength) {
+                console.log(`temp: ${temp}, previous: ${previous}, next: ${next}`);
+                if (!firstHit) temp += next.length;
+                if (firstHit) temp += previous.length + next.length;
+                break;
+            }
+
             if (previous === next) {
                 if (firstHit) {
-                    firstHit = false;
-                    temp += previous.length + 1;  // 1 means count number
+                    temp += previous.length + 1;
                 }
-            } else {
-                previous = next;
-                temp++;
+                firstHit = false;
+                pointer += previous.length - 1  // for문에서 +1 증가하기 때문;
+            }
+
+            if (previous !== next) {
+                if(!firstHit) {
+                    pointer += previous.length - 1  // for문에서 +1 증가하기 때문;
+                }
+                else temp++;
                 firstHit = true;
             }
+
         }
-        console.log(`charLength(${charLength}): ${temp}`);
+
+        console.log(charLength + ' ' + temp);
         min = Math.min(min, temp);
     }
+
     return min;
 }
 
-console.log(solution('abcdxyzxyz'));
+console.log(solution('xababcdcdababcdcd'));

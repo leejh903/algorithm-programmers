21a78e5b8816980015c02f636d5802fc2a92c5da
Brad Lee <brad903@naver.com>
2019-10-05 14:09:54 +0900

    kakao 2020 60057 solved another way but failed

--- programmers-vscode/2020_kakao_blind/p60057/index.js
+++ programmers-vscode/2020_kakao_blind/p60057/index.js
@@ -4,41 +4,32 @@ function solution(s) {
     for (let charLength = 1; charLength <= Math.floor(s.length / 2); charLength++) {
 
         let temp = 0;
-        let firstHit = true;
-        for (let pointer = 0; pointer < s.length; pointer++) {
-            let previous = s.slice(pointer, pointer + charLength)
-            let next = s.slice(pointer + charLength, pointer + (charLength * 2));
+        let hit = 0;
+        let previous = '';
+        for (let pointer = 0; pointer < s.length; pointer += charLength) {
+            let next = s.slice(pointer, pointer + charLength);
 
-            if (next.length < charLength) {
-                console.log(`temp: ${temp}, previous: ${previous}, next: ${next}`);
-                if (!firstHit) temp += next.length;
-                if (firstHit) temp += previous.length + next.length;
-                break;
-            }
+            console.log(`temp: ${temp}, previous: ${previous}, next: ${next}`);
 
             if (previous === next) {
-                if (firstHit) {
-                    temp += previous.length + 1;
-                }
-                firstHit = false;
-                pointer += previous.length - 1  // for문에서 +1 증가하기 때문;
+                hit++;
             }
 
             if (previous !== next) {
-                if(!firstHit) {
-                    pointer += previous.length - 1  // for문에서 +1 증가하기 때문;
-                }
-                else temp++;
-                firstHit = true;
+                previous = next;
+                temp += next.length;
+                
+                if(hit != 0) temp += String(hit).length;
+                hit = 0;
             }
 
         }
+        if(hit != 0) temp += String(hit).length;
 
-        console.log(charLength + ' ' + temp);
         min = Math.min(min, temp);
     }
 
     return min;
 }
 
-console.log(solution('xababcdcdababcdcd'));
+console.log(solution('aaaaaaaaaaaaaaaaaaa'));

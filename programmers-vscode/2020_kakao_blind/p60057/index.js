b68d55589cdd362a8a7d7bcb504f3d6872beff6d
Brad Lee <brad903@naver.com>
2019-10-05 14:49:46 +0900

    kakao2020 60057 solved

--- programmers-vscode/2020_kakao_blind/p60057/index.js
+++ programmers-vscode/2020_kakao_blind/p60057/index.js
@@ -3,33 +3,33 @@ function solution(s) {
 
     for (let charLength = 1; charLength <= Math.floor(s.length / 2); charLength++) {
 
-        let temp = 0;
-        let hit = 0;
-        let previous = '';
-        for (let pointer = 0; pointer < s.length; pointer += charLength) {
+        let hit = 1;
+        let previous = s.slice(0, charLength);
+        let newStr = '';
+        for (let pointer = charLength; pointer < s.length; pointer += charLength) {
             let next = s.slice(pointer, pointer + charLength);
-
-            console.log(`temp: ${temp}, previous: ${previous}, next: ${next}`);
 
             if (previous === next) {
                 hit++;
             }
 
             if (previous !== next) {
+                if (hit != 1) newStr += (String(hit) + previous);
+                else newStr += previous;
+
                 previous = next;
-                temp += next.length;
-                
-                if(hit != 0) temp += String(hit).length;
-                hit = 0;
+                hit = 1;
             }
 
         }
-        if(hit != 0) temp += String(hit).length;
+        if (hit != 1) newStr += (String(hit) + previous);
+        else newStr += previous;
 
-        min = Math.min(min, temp);
+        // console.log(newStr);
+        min = Math.min(min, newStr.length);
     }
 
     return min;
 }
 
-console.log(solution('aaaaaaaaaaaaaaaaaaa'));
+// console.log(solution('xababcdcdababcdcd'));

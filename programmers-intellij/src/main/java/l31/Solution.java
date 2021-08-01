421938d43bb2a169d511dfa6fbc38a4e226e5d5a
이정현(brandon) <brad903@naver.com>
2021-08-01 19:17:08 +0900

    add explanation about l31

--- programmers-intellij/src/main/java/l31/Solution.java
+++ programmers-intellij/src/main/java/l31/Solution.java
@@ -1,5 +1,8 @@
 package l31;
 
+// 1. 앞에 값이 그 뒤의 값보다 작은 것(i번째)을 찾는다
+// 2. 그 작은 값(i번째)부터 그 뒤의 값 중 그 수보다 큰 값(j번째) 찾아 swap 한다
+// 3. i번째 뒤의 값들은 뒤에 나올 수 있는 수 중에서 가장 값이므로 reverse 통해 가장 작은 값으로 만든다
 class Solution {
     public void nextPermutation(int[] nums) {
         int i = nums.length - 2;

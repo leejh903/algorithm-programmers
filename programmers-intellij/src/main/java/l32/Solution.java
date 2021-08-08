ef9118e5cd92260c0aed9f58c45fb3a7e6f46ccf
이정현(brandon) <brad903@naver.com>
2021-08-08 17:23:11 +0900

    reference leetcode 32 from another

--- programmers-intellij/src/main/java/l32/Solution.java
+++ programmers-intellij/src/main/java/l32/Solution.java
@@ -4,30 +4,27 @@ import java.util.Stack;
 
 class Solution {
     public int longestValidParentheses(String s) {
-        if(s.length() < 2) return 0;
-
+        Stack<Integer> stack = new Stack<>();
+        stack.add(-1);
         int answer = 0;
-        for (int start = 0; start < s.length() - 1; start++) {
-            for (int end = start + 1; end < s.length(); end++) {
-                if(isValid(s.substring(start, end + 1))) {
-                    answer = Math.max(answer, end - start + 1);
-                }
+
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                stack.push(i);
+                continue;
+            }
+
+            if(!stack.isEmpty()) {
+                stack.pop();
+            }
+
+            if(!stack.isEmpty()) {
+                answer = Math.max(answer, i - stack.peek());
+            } else {
+                stack.push(i);
             }
         }
         return answer;
     }
 
-    public boolean isValid(String s) {
-        Stack<Character> stack = new Stack();
-        for (char c : s.toCharArray()) {
-            if(c == '(') {
-                stack.push(c);
-                continue;
-            }
-
-            if(stack.isEmpty()) return false;
-            stack.pop();
-        }
-        return stack.isEmpty();
-    }
 }

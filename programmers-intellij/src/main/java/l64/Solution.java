96df31a4b367fc7e5fb1244ebcc044ecee2e3e4e
이정현(brandon) <brad903@naver.com>
2021-11-21 17:04:19 +0900

    try leecode l64, but time limit exceeded

---
+++ programmers-intellij/src/main/java/l64/Solution.java
@@ -0,0 +1,30 @@
+package l64;
+
+class Solution {
+	private int minSum = Integer.MAX_VALUE;
+
+	public int minPathSum(int[][] grid) {
+		dfs(grid, 0, 0, 0);
+		return minSum;
+	}
+
+	private void dfs(int[][] grid, int row, int col, int sum) {
+		// out of index
+		if (row >= grid.length || col >= grid[0].length) {
+			return;
+		}
+
+		sum += grid[row][col];
+		// reached bottom right
+		if (row == grid.length - 1 && col == grid[0].length - 1) {
+			minSum = Math.min(minSum, sum);
+			return;
+		}
+
+		// right
+		dfs(grid, row, col + 1, sum);
+
+		// below
+		dfs(grid, row + 1, col, sum);
+	}
+}

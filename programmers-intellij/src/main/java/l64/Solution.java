97d7a2cffb1c7bff94a9fbb3dcb0911f1579abf7
이정현(brandon) <brad903@naver.com>
2021-11-21 17:40:15 +0900

    solved leetcode l64 using dp and dfs

--- programmers-intellij/src/main/java/l64/Solution.java
+++ programmers-intellij/src/main/java/l64/Solution.java
@@ -1,34 +1,44 @@
 package l64;
 
 class Solution {
-	private int minSum = Integer.MAX_VALUE;
+	private int[][] visited;
 
 	public int minPathSum(int[][] grid) {
-		dfs(grid, 0, 0, 0);
-		return minSum;
+		visited = new int[grid.length][grid[0].length];
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				visited[i][j] = -1;
+			}
+		}
+		return dfs(grid, 0, 0);
 	}
 
-	private void dfs(int[][] grid, int row, int col, int sum) {
-		// finish meaningless recursive
-		if (sum >= minSum) {
-			return;
+	private int dfs(int[][] grid, int row, int col) {
+		// if visited before, it returns value which is calculated before
+		if (visited[row][col] != -1) {
+			return visited[row][col];
 		}
-		// out of index
-		if (row >= grid.length || col >= grid[0].length) {
-			return;
+
+		int sum = 0;
+		// reached bottom right
+		if (row == grid.length - 1 && col == grid[0].length - 1) {
+			return grid[row][col];
 		}
 
 		sum += grid[row][col];
-		// reached bottom right
-		if (row == grid.length - 1 && col == grid[0].length - 1) {
-			minSum = Math.min(minSum, sum);
-			return;
+		// right
+		int right = Integer.MAX_VALUE;
+		if (col + 1 < grid[0].length) {
+			right = dfs(grid, row, col + 1);
 		}
 
-		// right
-		dfs(grid, row, col + 1, sum);
-
 		// below
-		dfs(grid, row + 1, col, sum);
+		int below = Integer.MAX_VALUE;
+		if (row + 1 < grid.length) {
+			below = dfs(grid, row + 1, col);
+		}
+
+		visited[row][col] = sum + Math.min(right, below);
+		return visited[row][col];
 	}
 }

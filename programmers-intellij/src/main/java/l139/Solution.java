38305db8be29a7f98691c950eddce3b3e933c3fd
brad <brad903@naver.com>
2022-05-28 18:18:35 +0900

    solved leetcode problem l139

--- programmers-intellij/src/main/java/l139/Solution.java
+++ programmers-intellij/src/main/java/l139/Solution.java
@@ -5,27 +5,36 @@ import java.util.List;
 import java.util.Set;
 
 class Solution {
-    public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> dict = new HashSet<>();
-        for (String word : wordDict) {
-            dict.add(word);
-        }
+    Boolean[] dp;
 
-        return dfs(s, 0, dict, new StringBuilder());
+    public boolean wordBreak(String s, List<String> wordDict) {
+        dp = new Boolean[s.length()];
+        return dfs(s, 0, new HashSet<String>(wordDict));
     }
 
-    boolean dfs(String s, int index, Set<String> dict, StringBuilder sb) {
+    boolean dfs(String s, int index, Set<String> dict) {
+        if (index >= s.length()) {
+            return true;
+        }
+        if (dp[index] != null) {
+            return dp[index];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        dp[index] = false;
         for (int i = index; i < s.length(); i++) {
             sb.append(s.charAt(i));
 
+            // i + 1부터 새롭게 시작하는 케이스, 그냥 계속 덧붙여 나가는 케이스 모두 고려
             if (dict.contains(sb.toString())) {
-                if (dfs(s, i + 1, dict, sb)) {
-                    return true;
+                // i + 1부터 새롭게 시작하는 케이스
+                if (dfs(s, i + 1, dict)) {
+                    dp[index] = true;
+                    break;
                 }
-                sb = new StringBuilder();
             }
         }
 
-        return sb.length() == 0;
+        return dp[index];
     }
 }

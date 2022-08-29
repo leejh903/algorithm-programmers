7f8451e339ba703785b7cab6a15f73e9454a6223
brad <brad903@naver.com>
2022-08-30 01:30:34 +0900

    solved leetcode l1162 using hint

--- programmers-intellij/src/main/java/l1162/Solution.java
+++ programmers-intellij/src/main/java/l1162/Solution.java
@@ -1,95 +1,45 @@
 package l1162;
 
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Set;
 
 class Solution {
     private static final int[][] DIRECTIONS = new int[][] { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
     int answer = -1;
 
     public int maxDistance(int[][] grid) {
-        boolean hasLand = false;
+        Queue<int[]> queue = new LinkedList<>();
         for (int r = 0; r < grid.length; r++) {
             for (int c = 0; c < grid[0].length; c++) {
                 if (grid[r][c] == 1) {
-                    hasLand = true;
+                    queue.add(new int[] { r, c });
                 }
             }
         }
-        if (!hasLand)
-            return -1;
-
-        for (int r = 0; r < grid.length; r++) {
-            for (int c = 0; c < grid[0].length; c++) {
-                if (grid[r][c] == 0) {
-                    answer = Math.max(answer, bfs(grid, r, c));
-                }
-            }
-        }
-        return answer;
-    }
-
-    // bfs find nearlest land from row and col
-    int bfs(int[][] grid, int row, int col) {
-        Queue<Point> queue = new LinkedList<>();
-        Point start = new Point(row, col);
-        Set<Point> visited = new HashSet<>();
-        queue.add(start);
-        visited.add(start);
 
         int res = -1;
         while (!queue.isEmpty()) {
-            Point p = queue.poll();
+            int[] p = queue.poll();
+            int row = p[0];
+            int col = p[1];
+
             for (int i = 0; i < DIRECTIONS.length; i++) {
-                int newRow = p.row + DIRECTIONS[i][0];
-                int newCol = p.col + DIRECTIONS[i][1];
+                int newRow = p[0] + DIRECTIONS[i][0];
+                int newCol = p[1] + DIRECTIONS[i][1];
 
-                if (newRow < 0 || newRow >= grid.length || newCol < 0 || newCol >= grid[0].length) {
+                // grid[newRow][newCol] != 0 means it is already visited by other close land
+                if (newRow < 0 || newRow >= grid.length || newCol < 0 || newCol >= grid[0].length
+                        || grid[newRow][newCol] != 0) {
                     continue;
                 }
-                if (visited.contains(new Point(newRow, newCol))) {
-                    continue;
-                }
 
-                if (grid[newRow][newCol] == 1) {
-                    return distance(col, row, newCol, newRow);
-                }
-                if (grid[newRow][newCol] == 0) {
-                    Point next = new Point(newRow, newCol);
-                    queue.add(next);
-                    visited.add(next);
-                }
+                grid[newRow][newCol] = grid[row][col] + 1;
+                res = Math.max(res, grid[newRow][newCol]);
+                queue.add(new int[] { newRow, newCol });
             }
         }
-        return res;
-    }
 
-    int distance(int x0, int y0, int x1, int y1) {
-        return Math.abs(x0 - x1) + Math.abs(y0 - y1);
-    }
-
-}
-
-class Point {
-    int row;
-    int col;
-
-    public Point(int row, int col) {
-        this.row = row;
-        this.col = col;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        boolean result = false;
-        if (object == null || object.getClass() != getClass()) {
-            result = false;
-        } else {
-            Point point = (Point) object;
-            result = (this.row == point.row && this.col == point.col);
-        }
-        return result;
+        // res -1 b/c it start from land(1)
+        return res == -1 ? -1 : res - 1;
     }
 }

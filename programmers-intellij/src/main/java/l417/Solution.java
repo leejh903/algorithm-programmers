6cad9bcc23a14afbe49b9d3e627a862094d37427
brad <brad903@naver.com>
2022-09-04 20:32:21 +0900

    sovled leetcode problem 417 refering discuss - think of start line the other way round - do not need to process state at once

--- programmers-intellij/src/main/java/l417/Solution.java
+++ programmers-intellij/src/main/java/l417/Solution.java
@@ -1,71 +1,52 @@
 package l417;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Solution {
     static final int[][] DIRECTIONS = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
-    int m, n;
+    int row, col;
 
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        m = heights.length;
-        n = heights[0].length;
+        row = heights.length;
+        col = heights[0].length;
+
+        boolean[][] pacific = new boolean[row][col];
+        boolean[][] atlantic = new boolean[row][col];
+
+        for (int i = 0; i < col; i++) {
+            dfs(heights, 0, i, Integer.MIN_VALUE, pacific);
+            dfs(heights, row - 1, i, Integer.MIN_VALUE, atlantic);
+        }
+
+        for (int i = 0; i < row; i++) {
+            dfs(heights, i, 0, Integer.MIN_VALUE, pacific);
+            dfs(heights, i, col - 1, Integer.MIN_VALUE, atlantic);
+        }
 
         List<List<Integer>> answer = new ArrayList<>();
-        for (int i = 0; i < heights.length; i++) {
-            for (int j = 0; j < heights[0].length; j++) {
-                boolean[] result = dfs(heights, i, j, new boolean[m][n]);
-                if (result[0] && result[1]) {
-                    answer.add(List.of(i, j));
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    answer.add(Arrays.asList(i, j));
                 }
             }
         }
         return answer;
     }
 
-    // return's first index is Pacific and second is Atlantic
-    boolean[] dfs(int[][] heights, int row, int col, boolean[][] visited) {
-        boolean[] result = new boolean[2];
-        if (isAdjacentToPacific(row, col)) {
-            result[0] = true;
+    void dfs(int[][] heights, int i, int j, int prev, boolean[][] ocean) {
+        if (i < 0 || i >= row || j < 0 || j >= col) {
+            return;
         }
-        if (isAdjacentToAtlantic(row, col)) {
-            result[1] = true;
+        if (heights[i][j] < prev || ocean[i][j]) {
+            return;
         }
 
-        for (int i = 0; i < DIRECTIONS.length; i++) {
-            int newRow = row + DIRECTIONS[i][0];
-            int newCol = col + DIRECTIONS[i][1];
-
-            if (newRow < 0 || newRow >= heights.length || newCol < 0 || newCol >= heights[0].length) {
-                continue;
-            }
-            if (visited[newRow][newCol]) {
-                continue;
-            }
-            if (heights[newRow][newCol] > heights[row][col]) {
-                continue;
-            }
-
-            visited[newRow][newCol] = true;
-            boolean[] res = dfs(heights, newRow, newCol, visited);
-            visited[newRow][newCol] = false;
-            if (res[0]) {
-                result[0] = true;
-            }
-            if (res[1]) {
-                result[1] = true;
-            }
+        ocean[i][j] = true;
+        for (int k = 0; k < DIRECTIONS.length; k++) {
+            dfs(heights, i + DIRECTIONS[k][0], j + DIRECTIONS[k][1], heights[i][j], ocean);
         }
-
-        return result;
-    }
-
-    boolean isAdjacentToPacific(int row, int col) {
-        return row == 0 || col == 0;
-    }
-
-    boolean isAdjacentToAtlantic(int row, int col) {
-        return row == m - 1 || col == n - 1;
     }
 }

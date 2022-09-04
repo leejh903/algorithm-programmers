329a0a8c1e658b0f2928794865ebf06fa39567b9
brad <brad903@naver.com>
2022-09-04 17:12:30 +0900

    solved leetcode problem l221 refering dp solution

--- programmers-intellij/src/main/java/l221/Solution.java
+++ programmers-intellij/src/main/java/l221/Solution.java
@@ -1,32 +1,19 @@
 package l221;
 
 class Solution {
+    int[][] dp;
     public int maximalSquare(char[][] matrix) {
+        dp = new int[matrix.length][matrix[0].length];
+
         int answer = 0;
         for (int r = 0; r < matrix.length; r++) {
             for (int c = 0; c < matrix[0].length; c++) {
-                if (matrix[r][c] != '1')
-                    continue;
-
-                int col = matrix[0].length;
-                int row = 0;
-                for (int r2 = r; r2 < matrix.length; r2++) {
-                    if (matrix[r2][c] == '0') {
-                        break;
-                    }
-
-                    int tempCol = 0;
-                    for (int c2 = c; c2 < matrix[0].length; c2++) {
-                        if (matrix[r2][c2] == '0')
-                            break;
-                        tempCol++;
-                    }
-                    col = Math.min(col, tempCol);
-                    row++;
-
-                    int len = Math.min(col, row);
-                    answer = Math.max(answer, len * len);
+                if(r == 0 || c == 0 || matrix[r][c] == '0') {
+                    dp[r][c] = matrix[r][c] - '0';
+                } else {
+                    dp[r][c] = Math.min(dp[r-1][c-1], Math.min(dp[r-1][c], dp[r][c-1])) + 1;
                 }
+                answer = Math.max(answer, dp[r][c] * dp[r][c]);
             }
         }
         return answer;
